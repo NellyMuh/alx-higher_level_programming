6f038129aeff37e53a42f9df42d38143614ebe4d
NellyMuh <m.nelly704@gmail.com>
2023-01-09 10:43:31 -0800

    file created

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if type(my_list) == list:
+        cp_list = my_list.copy()
+    if idx < 0 or idx > len(cp_list)-1:
+        return cp_list
+    else:
+        cp_list[idx] = element
+        return cp_list
